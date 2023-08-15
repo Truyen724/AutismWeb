c853c41f22637870652c049175999d2ecfa7bb42
Nguyễn Truyền <truyenlol714@gmail.com>
2023-08-16 00:15:10 +0700

    them moi

--- server_python/main.py
+++ server_python/main.py
@@ -1,27 +1,42 @@
 
 import pandas as pd
 import numpy as np
-from numpy import dot
 from datetime import datetime
 from numpy.linalg import norm
 # from get_imotion import get_imotion
 from flask_cors import CORS
-from detect import detect_face_base64_mask
+import cv2
 from flask import Flask, request
 import time
-
+from flask_wtf.csrf import CSRFError
+from flask_wtf.csrf import CSRFProtect
+import json
+import os
+from PIL import Image
+import io
 
 # get_top_sim(10,api_string)
 app = Flask(__name__)
-
+csrf = CSRFProtect(app)
 CORS(app)
-@app.route('/predict', methods=['POST'])
+@app.route('/predict', methods=['POST', 'GET'])
+@csrf.exempt
 def make_prediction():
     program_starts = time.time()
     if request.method == 'POST':
-        dict = {}
 
-        return 5
+        gender = request.form.get('gender')
+        print(request.form.get('gender'))
+        f = request.files['photo']
+        image = f.read()
+        image_np = np.frombuffer(image, dtype=np.uint8)
+        image = cv2.imdecode(image_np, cv2.IMREAD_UNCHANGED)
+
+
+        cv2.imshow("x",image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        return "xin chao"
 
 
 if __name__ == '__main__':
--- server_python/test.py
+++ server_python/test.py
@@ -1,4 +1,9 @@
 from detect_autism import AutismDetect
 at = AutismDetect()
 
-at.runcam()
+import cv2
+img = cv2.imread('image/20.jpg')
+img2, pred = at.atism_detect(img)
+cv2.imshow("test", img2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
